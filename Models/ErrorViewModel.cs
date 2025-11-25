b675efb7416c60dbf90a3660e301064f9a328960
carla <carla@CARLA-OLTEANU>
2025-11-25 18:37:21 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Olteanu_Carla_Lab1_MPA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

a07b08b5bbfec236c6e701e23f8cfc0385714936
CARLA-ANTONIA OLTEANU <carla.olteanu@stud.ubbcluj.ro>
2025-11-25 19:32:09 +0200

    BunVenitController

---
+++ Controllers/BunVenitController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Olteanu_Carla_Lab1_MPA.Models;
+
+namespace Olteanu_Carla_Lab1_MPA.Controllers
+{
+    public class BunVenitController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+        public string AfiseazaDate(string nume, int varsta)
+        {
+            return $"Salut {nume}, ai {varsta} ani.";
+        }
+    }
+}
